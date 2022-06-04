6be6271162b495d617f01f40352b8ce1830fd884
PauloRSF <paulorsouzaf@gmail.com>
2022-06-04 12:59:08 -0300

    Create header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,1 +1,9 @@
-export const App = () => <></>;
+import { Page } from 'grommet';
+
+import { Header } from '../components/header';
+
+export const App = () => (
+  <Page fill>
+    <Header />
+  </Page>
+);
---
+++ src/components/header/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+
+import { Button, Header as GrommetHeader } from 'grommet';
+import { Download } from 'grommet-icons';
+
+export const Header: FC = () => (
+  <GrommetHeader background="brand" justify="end" pad={{ horizontal: 'large', vertical: 'small' }}>
+    <Button primary icon={<Download size="18px" />} label="Save" />
+  </GrommetHeader>
+);
